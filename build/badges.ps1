109c964bec3eb6bad138929c594fcda2a63216c3
Effie2096 <havealittlefaith2096@gmail.com>
2025-03-09 00:26:35 +0000

    added social links and about me

--- build/badges.ps1
+++ build/badges.ps1
@@ -1,31 +1,23 @@
-$badges80 = Get-ChildItem -Path $PSScriptRoot/"../assets/img/badges/80x15/"
-$badges150 = Get-ChildItem -Path $PSScriptRoot/"../assets/img/badges/150x20/"
-$badges88 = Get-ChildItem -Path $PSScriptRoot/"../assets/img/badges/88x31/"
-$stamps = Get-ChildItem -Path $PSScriptRoot/"../assets/img/badges/stamps/"
+$badges80 = "/assets/img/badges/80x15/"
+$badges150 = "/assets/img/badges/150x20/"
+$badges88 = "/assets/img/badges/88x31/"
+$stamps = "/assets/img/badges/stamps/"
+
 $outfile = $PSScriptRoot+"/../pages/badges.html"
 
 Clear-Content -Path $outfile
 
-Add-Content -Path $outfile -Value '<div id="smol" class="badge-box">'
-foreach ($badge in $badges80) {
-	Add-Content -Path $outfile -Value "<img src=`"/assets/img/badges/80x15/$($badge.Name)`" alt=`"$($badge.Name)`">"
-}
-Add-Content -Path $outfile -Value '</div>'
+Function AddImages {
+	param ($path, $id)
 
-Add-Content -Path $outfile -Value '<div id="skinny" class="badge-box">'
-foreach ($badge in $badges150) {
-	Add-Content -Path $outfile -Value "<img src=`"/assets/img/badges/150x20/$($badge.Name)`" alt=`"$($badge.Name)`">"
+	Add-Content -Path $outfile -Value "<div id=`"$id`" class=`"badge-box`">"
+	foreach ($badge in Get-ChildItem -Path $PSScriptRoot/"../"$path) {
+		Add-Content -Path $outfile -Value "<img src=`"$path$($badge.Name)`" alt=`"$($badge.Name)`">"
+	}
+	Add-Content -Path $outfile -Value '</div>'
 }
-Add-Content -Path $outfile -Value '</div>'
 
-Add-Content -Path $outfile -Value '<div id="normal" class="badge-box">'
-foreach ($badge in $badges88) {
-	Add-Content -Path $outfile -Value "<img src=`"/assets/img/badges/88x31/$($badge.Name)`" alt=`"$($badge.Name)`">"
-}
-Add-Content -Path $outfile -Value '</div>'
-
-Add-Content -Path $outfile -Value '<div id="stamps" class="badge-box">'
-foreach ($badge in $stamps) {
-	Add-Content -Path $outfile -Value "<img src=`"/assets/img/badges/stamps/$($badge.Name)`" alt=`"$($badge.Name)`">"
-}
-Add-Content -Path $outfile -Value '</div>'
+AddImages $badges80 "smol"
+AddImages $badges150 "skinny"
+AddImages $badges88 "normal"
+AddImages $stamps "stamps"
