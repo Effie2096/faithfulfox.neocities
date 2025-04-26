c6323fbd35ba37728e960ec772334a73bc6e69fa
Effie2096 <havealittlefaith2096@gmail.com>
2025-04-26 20:47:29 +0100

    revamped friends page

---
+++ script/friends.js
@@ -0,0 +1,36 @@
+const friends = document.querySelectorAll(".friend-tab").forEach((friend) => {
+	friend.addEventListener("click", () => {
+		document.querySelectorAll(".friend-page").forEach((page) => {
+			page.classList.remove("active")
+		})
+		document.querySelectorAll(".personal-board").forEach((board) => {
+			board.classList.remove("active")
+		})
+		addPins()
+		document.querySelector(`#${friend.id}-page`).classList.add("active")
+		document.querySelector(`#${friend.id}-board`).classList.add("active")
+	})
+})
+
+function addPins() {
+	document.querySelectorAll(".personal-board .pin").forEach((pin) => {
+		if (!pin.querySelector(".pin-img")) {
+			const pinImg = document.createElement("img")
+			pinImg.classList.add("pin-img")
+			pinImg.src = "/assets/img/pin.png"
+
+			pinImg.style.position = "absolute"
+			pinImg.style.width = "2rem"
+			pinImg.style.height = "2rem"
+			pinImg.style.top = "-0.4rem"
+			pinImg.style.left = "50%"
+			pinImg.style.transform = "translateX(-50%)"
+
+			pin.appendChild(pinImg)
+		}
+	})
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	addPins()
+})
