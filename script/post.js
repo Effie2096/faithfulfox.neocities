3d9800457eef6bc5809263c68d356c935810c127
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-06 12:46:28 +0100

    refactor(posts): move post relevant js to post.js

--- script/post.js
+++ script/post.js
@@ -28,3 +28,50 @@ function getCount(parent, recursive) {
 	}
 	return relevantChildren
 }
+
+function getContentTagDropdowns() {
+	var postContainer = document.getElementById("posts")
+	var tagDropdowns = Array.from(
+		postContainer.querySelectorAll(".content-card"),
+	).map((post) => {
+		var footerLeft = post
+			.getElementsByClassName("content-footer")[0]
+			.getElementsByClassName("content-footer-left")[0]
+		return footerLeft.getElementsByClassName("tag-dropdown")[0]
+	})
+	return tagDropdowns
+}
+
+function countContentTags() {
+	const tagDropdowns = getContentTagDropdowns()
+	tagDropdowns.forEach((tagDropdown) => {
+		const tagCountFound = tagDropdown.getElementsByClassName("tag-count")
+		if (tagCountFound.length > 0) {
+			tagCountFound[0].remove()
+		}
+
+		var tags = tagDropdown.getElementsByClassName("tag")
+		const tagDropdownWidth = tagDropdown.offsetWidth - 30
+
+		var rowWidth = 0
+		var tagCount = 0
+
+		for (var i = 0; i < tags.length; i++) {
+			const tagWidth = tags[i].offsetWidth
+
+			rowWidth += tagWidth
+
+			if (rowWidth < tagDropdownWidth) {
+				tagCount += 1
+				continue
+			}
+			break
+		}
+		if (tags.length - tagCount > 0) {
+			var tagCountBadge = document.createElement("div")
+			tagCountBadge.classList.add("tag-count")
+			tags[tagCount - 1].insertAdjacentElement("afterend", tagCountBadge)
+			tagCountBadge.innerHTML = tags.length - tagCount
+		}
+	})
+}
