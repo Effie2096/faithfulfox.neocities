b3fb67d5e00a99061ef1dc3a201ee072c464c279
effie <havealittlefaith2096@gmail.com>
2026-05-01 04:54:05 +0100

    feat(friends): bubbles canvas

--- script/friends.js
+++ script/friends.js
@@ -1,5 +1,7 @@
 import { sounds } from "./sounds.js";
 
+let selected = document.querySelector(".friend-tab").id;
+
 document.querySelectorAll(".friend-tab").forEach((friend) => {
 	friend.addEventListener("click", () => {
 		const friend_page = document.querySelector(`#${friend.id}-page`);
@@ -15,6 +17,11 @@ document.querySelectorAll(".friend-tab").forEach((friend) => {
 
 		friend_page.classList.add("active");
 		document.querySelector(`#${friend.id}-board`).classList.add("active");
+
+		selected = friend.id;
+
+		document.querySelector("#container").style.backgroundImage =
+			`url('/assets/img/friends/${friend.id}/background.png`;
 
 		sounds.swoosh_fast.audio.play();
 	});
@@ -63,43 +70,93 @@ document.addEventListener("DOMContentLoaded", () => {
 	const bubbles = [];
 
 	function createBubble(scale) {
-		const s = Math.min(scale * 100, 50);
+		const s = 100 * scale;
 		const bubble = {
 			x: Math.random() * window.innerWidth,
 			y: window.innerHeight + s,
-			scale: s,
+			scale: scale,
+			rotation: 0.0,
 		};
 		bubbles.push(bubble);
 	}
 
-	function drawBubble(x, y, scale) {
+	function drawBubble(bubble) {
+		let gradient = ctx.createRadialGradient(
+			bubble.x + (100 * bubble.scale) / 4,
+			bubble.y + (100 * bubble.scale) / 4,
+			(bubble.scale * 100) / 2,
+			bubble.x,
+			bubble.y,
+			bubble.scale * 100,
+		);
+		gradient.addColorStop(0, `rgba(225, 163, 1, 0.15)`);
+		gradient.addColorStop(0.7, "rgba(255,255,255, 0");
+		gradient.addColorStop(0.85, "rgba(255,255,255, 0");
+		gradient.addColorStop(0.9, "rgba(218, 84, 0, 0.2)");
+		ctx.fillStyle = gradient;
 		ctx.beginPath();
-		ctx.arc(x, y, scale, 0, 2 * Math.PI);
-		ctx.stroke();
+		ctx.arc(bubble.x, bubble.y, 100 * bubble.scale, 0, 2 * Math.PI);
+		ctx.fill();
+
+		const glint_x = bubble.x - (100 * bubble.scale) / 2;
+		const glint_y = bubble.y - (100 * bubble.scale) / 2;
+		const glint_size = (bubble.scale * 100) / 4;
+		gradient = ctx.createRadialGradient(
+			glint_x,
+			glint_y,
+			glint_size * 0.7,
+			glint_x,
+			glint_y,
+			glint_size,
+		);
+
+		gradient.addColorStop(0, "rgba(225, 183, 18, 0.15)");
+		gradient.addColorStop(0.85, "rgba(218, 84, 0, 0.01)");
+		ctx.fillStyle = gradient;
+		ctx.beginPath();
+		ctx.ellipse(
+			glint_x,
+			glint_y,
+			glint_size / 2,
+			glint_size,
+			Math.PI / 3,
+			0,
+			2 * Math.PI,
+		);
+		ctx.fill();
 	}
 
+	let t = 0;
 	function updateBubbles() {
 		for (let i = 0; i < bubbles.length; i++) {
 			const bubble = bubbles[i];
-			bubble.y -= 10;
+			t += i;
 
-			if (bubble.y + bubble.scale < window.innerHeight / 2) {
+			bubble.y -= 10 * bubble.scale;
+			const freq = bubble.scale;
+
+			bubble.x +=
+				bubble.scale < 0.5
+					? Math.sin(Math.PI * 4 * ((freq + 1) * 2) * t) * 1.5
+					: Math.sin(Math.PI * 2 * freq * t) * 2;
+			if (bubble.y + bubble.scale * 100 < 0) {
 				bubbles.splice(i, 1);
 				i--;
 			}
 		}
 	}
+
 	function drawBubbles() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		for (let i = 0; i < bubbles.length; i++) {
 			const bubble = bubbles[i];
 
 			ctx.save();
-			ctx.translate(bubble.x, bubble.y);
-			drawBubble(bubble.x, bubble.y, bubble.scale);
+			drawBubble(bubble);
 			ctx.restore();
 		}
 	}
+
 	let start = null;
 	requestAnimationFrame(firstFrame);
 	function firstFrame(timestamp) {
@@ -117,7 +174,11 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	setInterval(() => {
-		createBubble(Math.random());
-		console.log(bubbles);
-	}, 2000);
+		switch (selected) {
+			case "shyni": {
+				createBubble(Math.random());
+				break;
+			}
+		}
+	}, 200);
 });
